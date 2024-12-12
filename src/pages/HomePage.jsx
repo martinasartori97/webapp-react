7c4091db69bc348adb50f5d5a8ce84d1a34b00de
martinasartori97 <martinasartori9@gmail.com>
2024-12-12 14:58:19 +0100

    edit detailspage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/movies")
+            .then((response) => response.json())
+            .then((data) => setMovies(data))
+            .catch((error) => console.error("Errore:", error));
+    }, []);
 
 
 
-    const movies = [
+
+
+    const exampleMovies = [
         {
             id: 1,
             title: "Inception",
@@ -67,7 +80,7 @@ export default function HomePage() {
             <h1 className="my-4">home</h1>
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
                 {
-                    movies.map(movie => (
+                    exampleMovies.map(movie => (
                         <div className="col" key={movie.id}>
                             <div className="card bg-info">
                                 <div className="card-body">
